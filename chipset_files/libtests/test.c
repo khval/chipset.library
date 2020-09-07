4fc50461bd5e459ffb600205e5dbaa30ef276750
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2020-09-07 22:35:25 +0200

    Library open test.

---
+++ chipset_files/libtests/test.c
@@ -0,0 +1,58 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+
+BOOL open_lib( const char *name, int ver , const char *iname, int iver, struct Library **base, struct Interface **interface)
+{
+
+	*interface = NULL;
+	*base = IExec->OpenLibrary( name , ver);
+
+	if (*base)
+	{
+		 *interface = IExec->GetInterface( *base,  iname , iver, TAG_END );
+		if (!*interface) if (IDOS) IDOS -> Printf("Unable to getInterface %s for %s %d!\n",iname,name,ver);
+	}
+	else
+	{
+	   	if (IDOS) IDOS -> Printf("Unable to open the %s %ld!\n",name,ver);
+	}
+
+	return (*interface) ? TRUE : FALSE;
+}
+
+struct chipsetIFace * Ichipset = NULL;
+struct Library *chipsetBase = NULL;
+
+BOOL init()
+{
+	if ( ! open_lib( "chipset.library", 53L , "main", 1, &chipsetBase, (struct Interface **) &Ichipset  ) ) return FALSE;
+	return TRUE;
+}
+
+#define close_lib(b,i)			\
+	if (b) IExec->CloseLibrary(b);	\
+	if (i) IExec->DropInterface( (struct Interface *) i );	\
+	b = NULL; i = NULL;	
+
+void close_libs()
+{
+	close_lib( chipsetBase, Ichipset );
+}
+
+int main()
+{
+	if (init())
+	{
+		printf("its open, not bad\n");
+		printf("press enter to exit\n");
+		getchar();
+	}
+	close_libs();
+
+	return 0;
+}
+
+
